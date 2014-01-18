9d92676d579db1921ee50afb106ed66ed09c6b58
Alexander Schulz <alex.atomic@hotmail.de>
2014-01-18 16:45:48 +0100

    For great justice.

--- spacecp.sh
+++ spacecp.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Dem keys!!
-SPACECP_APIKEY="yoIneeddis"
+SPACECP_APIKEY=""
 # Mmh more tasty variables...
 SPACECP_URL="http://spacecp.net"
 # If you haven't guessed already, I'm going to declare all the important variables here
@@ -35,6 +35,7 @@ fi; fi
 
 ultima_yes=0 # Never say no! ...or was it never...
 force_update=0 # 0 nothing, 1 update, 2 install
+dn='/dev/null'
 
 #UUUHM plz dont change dis 1 kthxX
 OPTIND=1
@@ -50,27 +51,32 @@ __ () {
 ## CAUTION
 ## Shitty-ass arguments handling incoming!!
 ## CAUTION
-while getopts "h?yuir:c:a:j:p:" opt
+while getopts "h?yuir:c:C:k:a:j:p:" opt
 do
   case "$opt" in
   h|\?) show_help; exit 0;;
   y) ultima_yes=1;;
   u) force_update=1;;
   i) force_update=2;;
-  r) if [ -f "$OPTARG" ]
+  r) if [ -s "$OPTARG" ]
      then SPACECP_RTKJAR=$OPTARG
      else printf '%s\n' "'$OPTARG' is not a valid RTK jar."; exit 1
      fi;;
-  c) if [ -f "$OPTARG" ]
+  c) if [ -s "$OPTARG" ]
+     then SPACECP_CONFFILE=$OPTARG
+     else printf '%s\n' "'$OPTARG' is not a valid configuration file."; exit 1
+     fi;;
+  C) if [ -s "$OPTARG" ]
      then SPACECP_PROPFILE=$OPTARG
      else printf '%s\n' "'$OPTARG' is not a valid properties file."; exit 1
      fi;;
+  k) SPACECP_APIKEY=$(printf '%s' "$OPTARG" | tr '[:upper:]' '[:lower:]');;
   a) SPACECP_SERVAPI=$OPTARG;;
-  j) if [ -f "$OPTARG" ]
+  j) if [ -s "$OPTARG" ]
      then SPACECP_SERVJAR="$OPTARG"
      else printf '%s\n' "'$OPTARG' is not a valid server jar/file."; exit 1
      fi;;
-  p) if expr match "$OPTARG" '^[0-9]\+$' && [ "$OPTARG" -le 65535 -a "$OPTARG" -ge 1 ]
+  p) if expr match "$OPTARG" '^[0-9]\+$' >$dn && [ "$OPTARG" -le 65535 -a "$OPTARG" -ge 1 ]
      then SPACECP_PORT="$OPTARG"
      else printf '%s\n' "'$OPTARG' is not a valid port number."; exit 1
      fi;;
@@ -93,23 +99,32 @@ do
   --install) force_update=2;;
   --server-api=*) SPACECP_SERVAPI="$args";;
   --rtk-jar=*)
-    if [ $force_update -ne 2 -a -f "$args" ]
+    if [ $force_update -ne 2 -a -s "$args" ]
     then SPACECP_RTKJAR="$args"
     else
       printf '%s\n' "'$args' is not a valid RTK jar."
       exit 1
     fi
     ;;
+  --config=*)
+    if [ $force_update -ne 2 -a -s "$args" ]
+    then SPACECP_CONFFILE="$args"
+    else
+      printf '%s\n' "'$args' is not a valid configuration file."
+      exit 1
+    fi
+    ;;
   --properties=*)
-    if [ $force_update -ne 2 -a -f "$args" ]
+    if [ $force_update -ne 2 -a -s "$args" ]
     then SPACECP_PROPFILE="$args"
     else
       printf '%s\n' "'$args' is not a valid properties file."
       exit 1
     fi
     ;;
+  --api-key=*) SPACECP_APIKEY=$(printf '%s' "$args" | tr '[:upper:]' '[:lower:]');;
   --server-jar=*)
-    if [ $force_update -ne 2 -a -f "$args" ]
+    if [ $force_update -ne 2 -a -s "$args" ]
     then SPACECP_SERVJAR="$args"
     else
       printf '%s\n' "'$args' is not a valid server jar/file."
@@ -117,14 +132,14 @@ do
     fi
     ;;
   --port=*)
-    if expr match "$args" '^[0-9]\+$' && [ "$args" -le 65535 -a "$args" -ge 1 ]
+    if expr match "$args" '^[0-9]\+$' >$dn && [ "$args" -le 65535 -a "$args" -ge 1 ]
     then SPACECP_PORT="$args"
     else
       printf '%s\n' "'$args' is not a valid port number."
       exit 1
     fi
     ;;
-  "$_____") __;;
+  "$_____") __ && exit 0;;
   esac
 done
 ## Wow fucking arguments handling man
@@ -138,58 +153,58 @@ install_spacecp () {
   ## Installing SpaceCP
   ## Checks for already existing files to not make the user redownload everything over an over again if he calls the
   ##  script wrongly or has some faulty values at first.
-  printf '%s' "Getting configuration..."
+  printf '%s' "[     ] Getting configuration..."
   spacecp_conf=$(curl -sLA "SpaceCP Script $SPACECP______" "$SPACECP_SERVAPI/$SPACECP_APIKEY/config.yml")
-  [ -z "$spacecp_conf" ] && (printf ' [ERROR]\n%s\n' "Could not fetch the configuration under '$SPACECP_SERVAPI'.")
+  [ -z "$spacecp_conf" ] && (printf '\r[ERROR] \n%s\n' "Could not fetch the configuration under '$SPACECP_SERVAPI'.")
   echo "$spacecp_conf" > "$SPACECP_CONFFILE"
-  [ -s "$SPACECP_CONFFILE" ] || (printf ' [ERROR]\n%s\n' "Could not write to '$SPACECP_CONFFILE'." && exit 1)
-  printf '%s\n' " [OK]"
-  printf '%s' "Getting properties..."
+  [ -s "$SPACECP_CONFFILE" ] || (printf '\r[ERROR] \n%s\n' "Could not write to '$SPACECP_CONFFILE'." && exit 1)
+  printf '\r%s\n' "[OK]    "
+  printf '%s' "[     ] Getting properties..."
   spacecp_prop=$(curl -sLA "SpaceCP Script $SPACECP______" "$SPACECP_SERVAPI/$SPACECP_APIKEY/properties")
-  [ -z "$spacecp_prop" ] && (printf ' [ERROR]\n%s\n' "Could not fetch the properties under '$SPACECP_SERVAPI'.")
+  [ -z "$spacecp_prop" ] && (printf '\r[ERROR] \n%s\n' "Could not fetch the properties under '$SPACECP_SERVAPI'.")
   echo "$spacecp_prop" > "$SPACECP_PROPFILE"
-  [ -s "$SPACECP_PROPFILE" ] || (printf ' [ERROR]\n%s\n' "Could not write to '$SPACECP_PROPFILE'." && exit 1)
-  printf '%s\n' " [OK]"
-  printf '%s' "$SPACECP_SERVJAR..."
+  [ -s "$SPACECP_PROPFILE" ] || (printf '\r[ERROR] \n%s\n' "Could not write to '$SPACECP_PROPFILE'." && exit 1)
+  printf '\r%s\n' "[OK]    "
+  printf '%s' "[     ] $SPACECP_SERVJAR..."
   if ! [ -s "$SPACECP_SERVJAR" ]
   then
     curl -sLA "SpaceCP Script $SPACECP______" \
     "$SPACECP_GDNAPIURL/$(urlencode "$(basename "$SPACECP_SERVJAR")")" -o "$SPACECP_SERVJAR" \
-    || (printf ' [ERROR]\n%s\n' \
+    || (printf '\r[ERROR] \n%s\n' \
         "Could not fetch the server jar '$(basename "$SPACECP_SERVJAR")' from SpaceGDN under '$SPACECP_GDNAPIURL'." \
         && exit 1)
   fi
-  printf '%s' "$SPACECP_RTKJAR..."
-  printf '%s\n' " [OK]"
+  printf '\r%s\n' "[OK]    "
+  printf '%s' "[     ] $SPACECP_RTKJAR..."
   if ! [ -s "$SPACECP_RTKJAR" ]
   then
     curl -sLA "SpaceCP Script $SPACECP______" \
     "$SPACECP_DLAPIURL/$(urlencode "$(basename "$SPACECP_RTKJAR")")" -o "$SPACECP_RTKJAR" \
-    || (printf ' [ERROR]\n%s\n' \
+    || (printf '\r[ERROR] \n%s\n' \
         "Could not download '$(basename "$SPACECP_RTKJAR")' from the download server under '$SPACECP_DLAPIURL'." \
         && exit 1)
   fi
-  printf '%s' "$SPACECP_RPJAR..."
-  printf '%s\n' " [OK]"
+  printf '\r%s\n' "[OK]    "
+  printf '%s' "[     ] $SPACECP_RPJAR..."
   if ! [ -s "$SPACECP_RPJAR" ]
   then
     curl -sLA "SpaceCP Script $SPACECP______" \
     "$SPACECP_DLAPIURL/$(urlencode "$(basename "$SPACECP_RPJAR")")" -o "$SPACECP_RPJAR" \
-    || (printf ' [ERROR]\n%s\n' \
+    || (printf '\r[ERROR] \n%s\n' \
         "Could not download '$(basename "$SPACECP_RPJAR")' from the download server under '$SPACECP_DLAPIURL'." \
         && exit 1)
   fi
-  printf '%s' "$SPACECP_SMJAR..."
-  printf '%s\n' " [OK]"
+  printf '\r%s\n' "[OK]    "
+  printf '%s' "[     ] $SPACECP_SMJAR..."
   if ! [ -s "$SPACECP_SMJAR" ]
   then
     curl -sLA "SpaceCP Script $SPACECP______" \
     "$SPACECP_DLAPIURL/$(urlencode "$(basename "$SPACECP_SMJAR")")" -o "$SPACECP_SMJAR" \
-    || (printf ' [ERROR]\n%s\n' \
+    || (printf '\r[ERROR] \n%s\n' \
         "Could not download '$(basename "$SPACECP_SMJAR")' from the download server under '$SPACECP_DLAPIURL'." \
         && exit 1)
   fi
-  printf '%s\n' " [OK]"
+  printf '\r%s\n' "[OK]    "
 }
 
 update_spacecp () {
@@ -197,10 +212,28 @@ update_spacecp () {
          | tr '[:upper:]' '[:lower:]')
   wrapper_channel=$(cat "$SPACECP_CONFFILE" | sed -n '/^wrapper:$/,/^[^ ]\+/s/^  channel: \([a-zA-Z]\+\)/\1/p' \
                     | tr '[:upper:]' '[:lower:]')
-  server_channel=$(cat "$SPACECP_CONFFILE" | sed -n '/^server:$/,/^[^ ]\+/s/^  channel: \([a-zA-Z]\+\)/\1/p/' \
+  server_channel=$(cat "$SPACECP_CONFFILE" | sed -n '/^server:$/,/^[^ ]\+/s/^  channel: \([a-zA-Z]\+\)/\1/p' \
                    | tr '[:upper:]' '[:lower:]')
-  spacecp_channel=$(cat "$SPACECP_CONFFILE" | sed -n '/^spacecp:$/,/^[^ ]\+/s/^  channel: \([a-zA-Z]\+\)/\1/p/' \
+  server_build=$(cat "$SPACECP_CONFFILE" | sed -n '/^server:$/,/^[^ ]\+/s/^  build: \([a-zA-Z]\+\)/\1/p' \
+                 | tr '[:upper:]' '[:lower:]')
+  server_autoupdate=$(cat "$SPACECP_CONFFILE" | sed -n '/^server:$/,/^[^ ]\+/s/^  auto-update: \([a-zA-Z]\+\)/\1/p' \
+                      | tr '[:upper:]' '[:lower:]')
+  spacecp_channel=$(cat "$SPACECP_CONFFILE" | sed -n '/^spacecp:$/,/^[^ ]\+/s/^  channel: \([a-zA-Z]\+\)/\1/p' \
                     | tr '[:upper:]' '[:lower:]')
+  spacecp_build=$(cat "$SPACECP_CONFFILE" | sed -n '/^spacecp:$/,/^[^ ]\+/s/^  build: \([a-zA-Z]\+\)/\1/p' \
+                  | tr '[:upper:]' '[:lower:]')
+  spacecp_autoupdate=$(cat "$SPACECP_CONFFILE" | sed -n '/^spacecp:$/,/^[^ ]\+/s/^  auto-update: \([a-zA-Z]\+\)/\1/p' \
+                       | tr '[:upper:]' '[:lower:]')
+  [ -z "$SPACECP_APIKEY" ] && SPACECP_APIKEY=$(cat "$SPACECP_CONFFILE" \
+                                               | sed -n '/^spacecp:$/,/^[^ ]\+/s/^  apikey: \([a-zA-Z]\+\)/\1/p' \
+                                               | tr '[:upper:]' '[:lower:]')
+  if [ -z "$slug" ] || [ -z "$wrapper_channel" ] || [ -z "$server_channel" ] || [ -z "$server_build" ] \
+     || [ -z "$server_autoupdate" ] || [ -z "$spacecp_channel" ] || [ -z "$spacecp_build" ] \
+     || [ -z "$spacecp_autoupdate" ] || [ -z "$SPACECP_APIKEY" ]
+  then
+    printf '%s\n' "ERROR: Some variables could not be found in '$SPACECP_CONFFILE', your configuration may be damaged."
+    return 1
+  fi
   ## TO DO
   ## Do updating stuff
   ## lolwat gdn?
@@ -208,14 +241,14 @@ update_spacecp () {
 }
 
 start_spacecp () {
-  printf '%s' "Starting SpaceCP..."
+  printf '%s' "[     ] Starting SpaceCP..."
   ## Actually starting RTK now and checking the exit status.
   ## It is EXTREMELY important for the starting command to automatically fork itself into the background,
   ##  or else we can't correctly check if it started to begin with, and more importantly,
   ##  cannot send a POST request to the SpaceCP servers to notify them that the server started!
   if "$SPACECP_STARTCOMMAND" "$SPACECP_STARTARGS" "$SPACECP_RTKJAR" "$SPACECP_RTKARGS"
-  then printf '%s\n' " [$SPACECP_STARTCOMMAND]"
-  else printf ' [ERROR]\n%s\n' "Could not start '$SPACECP_RTKJAR' with $SPACECP_STARTCOMMAND."
+  then printf '\r%s\n' "[OK]    Starting SpaceCP... [$SPACECP_STARTCOMMAND]"
+  else printf '\r[ERROR]\n%s\n' "Could not start '$SPACECP_RTKJAR' with '$SPACECP_STARTCOMMAND'."
   fi
   curl -sLA "SpaceCP Script $SPACECP______" -X POST "$SPACECP_SERVAPI/$SPACECP_APIKEY/start"
 }
@@ -230,6 +263,39 @@ then
 fi
 SPACECP_APIKEY=$(urlencode "$SPACECP_APIKEY")
 
-## TO DO
-## start dem functions correctly and do the stuff around it
-## TO DO
+if [ -s "$SPACECP_CONFFILE" ]
+then
+  if ! update_spacecp
+  then
+    printf '%s' "Could not update SpaceCP. Start anyway? [Y/n] "
+    [ $ultima_yes -eq 1 ] && yn="y" || read yn
+    expr match "$yn" '^y.*' >$dn && yn=''
+  fi
+  if [ -z "$yn" ]
+  then
+    if ! start_spacecp
+    then
+      printf '%s\n' "Could not start SpaceCP."
+      exit 1
+    fi
+  fi
+else
+  printf '%s' "No SpaceCP configuration found. Install SpaceCP? [Y/n] "
+  [ $ultima_yes -eq 1 ] && (yn="y" && printf '\n') || read yn
+  expr match "$yn" '^y.*' >$dn && yn=''
+  if [ -z "$yn" ]
+  then
+    if install_spacecp
+    then
+      printf '%s\n' "SpaceCP installation succesfull. Starting SpaceCP for the first time."
+      if ! start_spacecp
+      then
+        printf '%s\n' "Could not start SpaceCP."
+        exit 1
+      fi
+    else
+      printf '%s\n' "Could not install SpaceCP."
+      exit 1
+    fi
+  fi
+fi
